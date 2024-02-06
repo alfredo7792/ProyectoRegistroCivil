4bfe3ac4355ea792d33bb2d0eb7cecb314a29f84
Stephano0207 <castanedastephano16@gmail.com>
2024-02-06 11:56:13 -0500

    Correcciones finales

--- resources/views/SubSistemaConsultas/ConsultaDNI/grafico.blade.php
+++ resources/views/SubSistemaConsultas/ConsultaDNI/grafico.blade.php
@@ -27,36 +27,37 @@
           <div class="col-4">
             <h5 style="font-size:19px; color:#004370">Leyenda del Grafico</h5>
            <div style="font-size:15px" class="text-body-secondary">
-            <p><i class="fa-solid fa-circle" style="color: #FFC107"></i> Pendiente</p>
-            <p><i class="fa-solid fa-circle" style="color: #0DCAF0"></i> En Proceso</p>
-            <p><i class="fa-solid fa-circle" style="color: #198754"></i> Aceptado o en agencia</p>
-            <p><i class="fa-solid fa-circle" style="color: #0969e7"></i> Entregado</p>
-            <p><i class="fa-solid fa-circle" style="color: #DC3545"></i> Rechazado</p>
+            <p><i class="fa-solid fa-circle" style="color: #FFC107"></i> Recibido</p> 
+            <p><i class="fa-solid fa-circle" style="color: #f04d0d"></i> En Revision</p> 
+            <p><i class="fa-solid fa-circle" style="color: #0DCAF0"></i> Aceptado</p> 
+            <p><i class="fa-solid fa-circle" style="color: #198754"></i> En Agencia</p>
+            <p><i class="fa-solid fa-circle" style="color: #0969e7"></i> Entregado</p> 
+            <p><i class="fa-solid fa-circle" style="color: #DC3545"></i> Rechazado</p> 
            </div>
           
           </div>
           <div class="col m-auto">
             
-            @if ($solicitud[0]->solEstado=="Pendiente")
+            @if ($solicitud[0]->solEstado=="Recibido")
             <div class="row  m-auto text-center"  style="color: #004370">
               <h5  style="font-size:19px">Estado de Tramite</h5>
-              <h1 style="font-size:70px">25%</h1>
+              <h1 style="font-size:90px">20%</h1>
             </div>
-            <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-              <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: 25%"></div>
+            <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: 20%"></div>
             </div>
             <div class="row m-auto text-center" style="color: #004370">
               <h3 style="font-size:17px">{{$solicitud[0]->solEstado}}</h3>
             </div>
             
             @else
-              @if ($solicitud[0]->solEstado=="En Proceso")
+              @if ($solicitud[0]->solEstado=="En Revision")
               <div class="row  m-auto text-center"  style="color: #004370">
                 <h5  style="font-size:19px">Estado de Tramite</h5>
-                <h1 style="font-size:70px">50%</h1>
+                <h1 style="font-size:90px">40%</h1>
               </div>
-              <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: 50%"></div>
+              <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped progress-bar-animated " style="width: 40%"></div>
               </div>
               <div class="row m-auto text-center" style="color: #004370">
                 <h3 style="font-size:17px">{{$solicitud[0]->solEstado}}</h3>
@@ -65,23 +66,23 @@
                   @if ($solicitud[0]->solEstado=="Aceptado")
                   <div class="row  m-auto text-center"  style="color: #004370">
                     <h5  style="font-size:19px">Estado de Tramite</h5>
-                    <h1 style="font-size:70px">75%</h1>
+                    <h1 style="font-size:90px">60%</h1>
                   </div>
-                  <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width: 75%"></div>
+                  <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: 60%"></div>
                   </div>
                   <div class="row m-auto text-center" style="color: #004370">
                     <h3 style="font-size:17px">{{$solicitud[0]->solEstado}}</h3>
                   </div>
                   @else
-                    @if ($solicitud[0]->solEstado=="Entregado")
+                    @if ($solicitud[0]->solEstado=="En Agencia")
                     <div class="row  m-auto text-center"  style="color: #004370">
                      
                       <h5  style="font-size:19px">Estado de Tramite</h5>
-                      <h1 style="font-size:70px">100%</h1>
+                      <h1 style="font-size:90px">80%</h1>
                     </div>
-                    <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                      <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" style="width: 100%"></div>
+                    <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width: 80%"></div>
 
                     </div>
                     <div class="row m-auto text-center" style="color: #004370">
@@ -89,9 +90,22 @@
                     </div>
                     
                     @else
+
+                    @if ($solicitud[0]->solEstado=="Entregado")
                     <div class="row  m-auto text-center"  style="color: #004370">
                       <h5  style="font-size:19px">Estado de Tramite</h5>
-                      <h1 style="font-size:70px">100%</h1>
+                      <h1 style="font-size:90px">100%</h1>
+                    </div>
+                    <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" style="width: 100%"></div>
+                    </div>
+                    <div class="row m-auto text-center" style="color: #004370">
+                      <h3 style="font-size:17px">{{$solicitud[0]->solEstado}}</h3>
+                    </div>
+                    @else
+                    <div class="row  m-auto text-center"  style="color: #004370">
+                      <h5  style="font-size:19px">Estado de Tramite</h5>
+                      <h1 style="font-size:90px">100%</h1>
                     </div>
                     <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
                       <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: 100%"></div>
@@ -100,10 +114,12 @@
                       <h3 style="font-size:17px">{{$solicitud[0]->solEstado}}</h3>
                     </div>
                     @endif
-                      
+                    
+                    @endif
+                   
                   @endif
               @endif
-                
+             
             @endif
            
           </div>
